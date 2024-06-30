995e2789734ff5a345aaae296bb8a6792ec7e391
tzh <3136374549@qq.com>
2024-06-30 11:19:32 +0800

    update

--- newpost.sh
+++ newpost.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-# 在 source/_posts 目录下创建一个新的 markdown 文件。目录为当前日期，文件名为随机字符串。
+# 在 source/_posts 目录下创建一个新的 markdown 文件。目录为当前日期，文件名为当前时刻。
 current_date=$(date "+%Y/%m/%d")
 filename=$(date "+%H%M%S")
 mkdir -p "$current_date"
-# random_filename=$(openssl rand -base64 6 | head -c 8)
 hexo new post -p "${current_date}/${filename}"
 echo "hexo creates a post at: ${current_date}/${filename}.md"
